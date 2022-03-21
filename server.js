152de692fc997f539b93622f0c457b9036170044
Danny Ramirez <danny.ramirezgd@gmail.com>
2022-03-21 14:23:19 -0400

    added comment model

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
     useUnifiedTopology: true
 });
 
-//Use this to log mongo queries being excuted
+//Use this to log mongo queries being executed
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
