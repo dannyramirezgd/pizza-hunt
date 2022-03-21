5cfbbe5121c022ced5e140434130b883403129bf
dannyrgd <danny.ramirezgd@gmail.com>
2022-03-21 18:37:04 -0400

    started to create reply schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,25 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId() 
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
 
 const CommentSchema = new Schema({
     writtenBy: {
@@ -10,7 +31,8 @@ const CommentSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    replies: [ReplySchema]
 });
 
 const Comment = model('Comment', CommentSchema);
